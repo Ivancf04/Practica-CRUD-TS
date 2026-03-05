bbd6f37ae84a4d43b2a333f7e521502b8cc34601
Ivancf04 <jcota_22@alu.uabcs.mx>
2026-03-04 22:14:40 -0700

    Creacion de carpeta src

---
+++ src/data/users.ts
@@ -0,0 +1,8 @@
+import { User } from "../models/users";
+
+const users: User[] = [
+    { id: 1, name: "Ivan", email: "ivan@ejemplo.com", password: 123456, birthday: "1990-01-01" },
+    { id: 2, name: "María", email: "maria@ejemplo.com", password: 123456, birthday: "1992-05-15" }
+];
+
+export default users;
